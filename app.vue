add0591d790c535e97304369718c8ab7833593de
wylited <44154103+wylited@users.noreply.github.com>
2023-05-28 13:58:47 +0800

    link to proper websites

--- app.vue
+++ app.vue
@@ -24,8 +24,8 @@
 
       <div class="leading-6 py-6">
         <h2 class="text-3xl text-primary py-3 underline">quick links</h2>
-        <p class="py-1"> &lt;-&lt; <a href="notes.wyli.tech"><strong>notes</strong></a> &gt;-&gt; </p>
-        <p> &lt;-&lt; <a href="blogs.wyli.tech"><strong>blogs</strong></a> &gt;-&gt; </p>
+        <p class="py-1"> &lt;-&lt; <a href="https://notes.wyli.tech"><strong>notes</strong></a> &gt;-&gt; </p>
+        <p> &lt;-&lt; <a href="https://blogs.wyli.tech"><strong>blogs</strong></a> &gt;-&gt; </p>
         <p> &lt;-&lt; <a href="/about"><strong>about</strong></a> &gt;-&gt; </p>
       </div>
     </Hero>
